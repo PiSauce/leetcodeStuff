5232c49eb97ea0e28f11ed7937a0090a4d7a9401
PiSauce <53952729+PiSauce@users.noreply.github.com>
2023-01-10 10:38:57 -0700

    solution finished

--- TwoSum.py
+++ TwoSum.py
@@ -1,1 +1,12 @@
 # Two Sum Problem from Leetcode
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        for i, j in ((x, y) for x in range(len(nums)) for y in range(len(nums))):
+            if i!=j and nums[i]+nums[j] == target:
+                return [i,j]
